fead5a955476708a3691fe937cd14b0085384a70
utsavized <utsavpandey@gmail.com>
2017-08-04 11:06:46 -0700

    Update routing

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import CreateGame from './CreateGame';
-import PlayGame from './PlayGame';
+import WaitGame from './WaitGame';
 import JoinGame from './JoinGame';
+import PlayGame from './PlayGame';
 import Home from './Home';
 
 export default class Main extends React.Component {
@@ -11,7 +12,8 @@ export default class Main extends React.Component {
             <Switch>
               <Route exact={true} path="/" component={Home}/>
               <Route exact={true} path="/create" component={CreateGame}/>
-              <Route path="/join" component={JoinGame}/>
+              <Route path="/wait/:guid/user/:user" component={WaitGame}/>
+              <Route path="/join/:guid" component={JoinGame}/>
               <Route path="/play/:guid" component={PlayGame}/>
             </Switch>
         );
