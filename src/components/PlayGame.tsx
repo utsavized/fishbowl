7cffc0549abcb15c64c6711bb8156f0baa1d2709
utsavized <utsavpandey@gmail.com>
2017-08-04 11:06:55 -0700

    Update play game

--- src/components/PlayGame.tsx
+++ src/components/PlayGame.tsx
@@ -4,11 +4,13 @@ import Game from '../models/Game';
 import GenericLocalStorageRepository from '../data/GenericLocalStorageRepository';
 import ILocalStorageRepository from '../interfaces/ILocalStorageRepository'; 
 
-export default class PlayGame extends React.Component<RouteComponentProps<any>, Game> { // tslint:disable-line
+export default class PLayGame extends React.Component<RouteComponentProps<any>, Game> { // tslint:disable-line
   private _gameRepository: ILocalStorageRepository<Game>; 
   constructor(props: RouteComponentProps<any>) { // tslint:disable-line
     super(props);
     this._gameRepository = new GenericLocalStorageRepository<Game>();
+    
+    this.getGame = this.getGame.bind(this);
     this.state = this.getGame(this.props.match.params.guid);
   }
 
@@ -17,17 +19,23 @@ export default class PlayGame extends React.Component<RouteComponentProps<any>, 
   }
 
   render() {
-    if (this.state.id === undefined) {
+    if (this.state.name === undefined) {
       return <div>Invalid game</div>;
     }
 
     return (
       <div>
-          <p>Playing {this.state.name},&nbsp;
-             created by {this.state.player.name}&nbsp;
-             with categories&nbsp;
-             {this.state.categories.map(category => <span key={category.name}>{category.name} </span>)}.
-          </p>
+          <p>Playing {this.state.name}</p>
+          Categories:
+          <ul>
+          {this.state.categories.map(category => <li key={category.name}>{category.name} </li>)}
+          </ul>
+          Joined players:
+          <ul>
+            {this.state.players.map(player => <li key={player.name}>{player.name} </li>)}
+          </ul>
+          <br />
+          <h2>Enjoy!</h2>          
       </div>
     );
   }
